af0de853f2c9bf0e63124163c770f92aff51be77
bambo9999 <1509567785@qq.com>
2023-05-13 19:32:50 +0800

    二分查找

---
+++ code/二分查找.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        flag = 0
+
+        while(low <= high):
+            mid = int((low + high) / 2)
+            if (nums[mid] == target):
+                flag = 1
+                return mid
+                break
+            elif (nums[mid] < target):
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        if(flag == 0):
+            return -1
